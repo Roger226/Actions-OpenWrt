05bb7d30543cb23b124cda99168d1ad8953e6a90
Roger226 <58210382+Roger226@users.noreply.github.com>
2020-02-16 20:53:51 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,6 +6,5 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-cd openwrt
-git clone https://github.com/rufengsuixing/luci-app-adguardhome package/lean/luci
+sed -i 's/10.0.1.254/192.168.50.5/g' package/base-files/files/bin/config_generate
+git clone https://github.com/rufengsuixing/luci-app-adguardhome package/lean/luci/luci-app-adguardhome
