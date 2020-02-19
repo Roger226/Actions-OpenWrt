c8b50ac7dd0d2a4fd3461c08f9fa47bd8e491d0e
Roger226 <58210382+Roger226@users.noreply.github.com>
2020-02-19 09:32:43 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,5 +6,5 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-sed -i 's/10.0.1.254/192.168.50.5/g' package/base-files/files/bin/config_generate
+# sed -i 's/10.0.1.254/192.168.50.5/g' package/base-files/files/bin/config_generate
 git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
