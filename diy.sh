05da4bb9766a5a40876d12b3539519391b4afdfb
Roger226 <58210382+Roger226@users.noreply.github.com>
2020-03-19 15:29:12 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -8,4 +8,5 @@
 # Modify default IP
 # sed -i 's/10.0.1.254/192.168.50.5/g' package/base-files/files/bin/config_generate
 git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
-git clone https://github.com/Lienol/openwrt-package package/passwall
+#git clone https://github.com/Lienol/openwrt-package package/passwall
+git clone https://github.com/Lienol/openwrt-package/tree/master/lienol/luci-app-passwall package/passwall
