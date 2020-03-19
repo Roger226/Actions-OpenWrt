c59f15c87c3c4094f83643f6698fa0f0955a838e
Roger226 <58210382+Roger226@users.noreply.github.com>
2020-03-19 21:25:39 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -9,4 +9,4 @@
 # sed -i 's/10.0.1.254/192.168.50.5/g' package/base-files/files/bin/config_generate
 git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
 #git clone https://github.com/Lienol/openwrt-package package/passwall
-git clone https://github.com/Lienol/openwrt-package/tree/master/lienol/luci-app-passwall package/passwall
+#git clone https://github.com/Lienol/openwrt-package/tree/master/lienol/luci-app-passwall package/passwall
